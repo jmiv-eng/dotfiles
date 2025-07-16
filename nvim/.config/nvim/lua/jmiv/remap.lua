e1a14b86d8592168f91c52c4a5111b102c5fe7a6
jmiv-eng <jami1063@colorado.edu>
2025-07-16 10:28:53 -0600

    neovim unbind help

--- nvim/.config/nvim/lua/jmiv/remap.lua
+++ nvim/.config/nvim/lua/jmiv/remap.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Disable help on F1, remap to F10
+vim.keymap.set({'n', 'i', 'v'}, '<F1>', '<Nop>')
+vim.keymap.set('n', '<F10>', ':help<CR>')
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -23,7 +27,6 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
